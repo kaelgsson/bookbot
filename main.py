250b0cb395f56c09a7c961103973a33009854ac8
kaelgsson <mikael.s.gustafsson@proton.me>
2025-12-26 19:24:54 +0100

    bookbot: 11 of 15

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_chars
 
 def get_book_text(file_path: str) -> str:
     with open(file_path) as f:
@@ -9,7 +9,9 @@ def main() -> None:
     book_path = "./books/frankenstein.txt"
     book_text = get_book_text(book_path)
     num_words = get_num_words(book_text)
+    num_chars = get_num_chars(book_text)
     print(f"Found {num_words} total words")
+    print(num_chars)
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -2,9 +2,19 @@ def get_num_words(text: str) -> int:
     number_of_words = len(text.split())
     return number_of_words
 
-__all__ = ["get_num_words"]
+def get_num_chars(text: str) -> dict:
+    char_dict = {}
+    for char in text:
+        _char = char.lower()
+        if _char in char_dict:
+            char_dict[_char] += 1
+        else:
+            char_dict[_char] = 1
+    return char_dict
+
+__all__ = ["get_num_words", "get_num_chars"]
 
 if __name__ == "__main__":
-    assert get_num_words("") == 0
     assert get_num_words("one two") == 2
-    print("stats.get_num_words: basic checks passed")
+    assert get_num_chars("on o") == {"o": 2, "n": 1, " ": 1}
+    print("basic checks passed")
